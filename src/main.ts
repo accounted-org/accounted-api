0a462e19e956ca4da43904a667f6a7d933fdd469
victorzix <linkgamesplays@gmail.com>
2026-01-29 22:45:33 -0300

    feat: add loger to main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import {HttpStatus, INestApplication, Logger, ValidationPipe} from '@nestjs/common';
 import helmet from 'helmet';
 
 class Main {
+  private readonly logger = new Logger('Main')
   constructor() {
     void this.boostrap();
   }
 
   private async boostrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
     this.setupSwagger(app);
     this.setupGlobalConfigs(app);
@@ -21,8 +22,8 @@ class Main {
     });
 
     await app.listen(String(process.env.PORT), () => {
-      console.log(`Server is running on http://localhost:${process.env.PORT}`);
-      console.log(
+      this.logger.debug(`Server is running on http://localhost:${process.env.PORT}`);
+      this.logger.debug(
         `API Documentation available at http://localhost:${process.env.PORT}/${process.env.DOCS_PREFIX}`,
       );
     });
