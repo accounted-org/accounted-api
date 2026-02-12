cc587a3b548115af7e815b29a3a05d2d527fe282
Pedro Braghin <braghindev@gmail.com>
2026-02-11 23:04:45 -0300

    fea(email): add integration with oracle provider

---
+++ src/modules/email/providers/oracle.provider.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { IEmailPayload, IEmailProvider } from './email.provider.interface';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class OracleEmailProvider implements IEmailProvider {
+  private transporter: nodemailer.Transporter;
+  private readonly emailDomain: string;
+
+  constructor(readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.ORACLE_SMTP_HOST,
+      port: Number(process.env.ORACLE_SMTP_PORT),
+      secure: false,
+      auth: {
+        user: process.env.ORACLE_SMTP_USER,
+        pass: process.env.ORACLE_SMTP_PASS,
+      },
+    });
+    this.emailDomain = String(configService.get('EMAIL_DOMAIN'));
+  }
+
+  async send(payload: IEmailPayload): Promise<string | undefined> {
+    const response = await this.transporter.sendMail({
+      from: `No-Reply <no-reply@${this.emailDomain}>`,
+      ...payload,
+    });
+
+    return response.id;
+  }
+}
